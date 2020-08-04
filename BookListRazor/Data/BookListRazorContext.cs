0ac0a971fe2d52aef9ae39605c6029643484f3ba
Jeffrey Soriano <jeffsor17@gmail.com>
2020-08-03 20:50:59 -0600

    added database and razor models

---
+++ BookListRazor/Data/BookListRazorContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using BookListRazor.Model;
+namespace BookListRazor.Data
+{
+    public class BookListRazorContext : DbContext
+    {
+        public BookListRazorContext(DbContextOptions<BookListRazorContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BookListRazor.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor
 {
@@ -23,7 +25,10 @@ namespace BookListRazor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
+
+            services.AddDbContext<BookListRazorContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("BookListRazorContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
